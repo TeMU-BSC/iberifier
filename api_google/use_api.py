49ba73ad4f2809f082c43e99df35c29a9d860dfa
BlancaCalvo <bcalvo.bsc@gmail.com>
2022-04-11 15:16:21 +0200

    factcheck apis

--- api_google/use_api.py
+++ api_google/use_api.py
@@ -1,21 +1,37 @@
 from googleapiclient.discovery import build
 import json
-import sys
+import argparse
+import importlib.util
+spec = importlib.util.spec_from_file_location("credentials", "/home/blanca/prova/credentials.py")
+credentials = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(credentials)
+google_api = credentials.google_api
 
-query = sys.argv[1]
 
-factCheckService = build("factchecktools", "v1alpha1", developerKey='AIzaSyAkulJMUBGrSBq0c5kBoZ-w97LpSiqPZus')
-request = factCheckService.claims().search(query=query,
-                                            #reviewPublisherSiteFilter="Maldita.es",
-                                            pageSize=1000000,#)
-                                           #query=query,
-                                           languageCode="pt")#,
-                                           #pageToken='CAs',
-                                           # maxAgeDays='',
-                                           #offset=10)
-                                           #reviewPublisherSiteFilter="Maldita.es",
-response = request.execute()
+def get_arguments(parser):
+    parser.add_argument("--query", default='vacunas', type=str, required=False)
+    return parser
 
-with open("results_{}.json".format(query), "w") as f:
-    f.write(json.dumps(response))
+def main():
+    parser = argparse.ArgumentParser()
+    parser = get_arguments(parser)
+    args = parser.parse_args()
+    print(args)
 
+    factCheckService = build("factchecktools", "v1alpha1", developerKey=google_api())
+    request = factCheckService.claims().search(query=args.query,
+                                                #reviewPublisherSiteFilter="Maldita.es",
+                                                pageSize=1000000,#)
+                                               #query=query,
+                                               languageCode="pt")#,
+                                               #pageToken='CAs',
+                                               # maxAgeDays='',
+                                               #offset=10)
+                                               #reviewPublisherSiteFilter="Maldita.es",
+    response = request.execute()
+
+    with open("results_{}.json".format(args.query), "w") as f:
+        f.write(json.dumps(response))
+
+if __name__ == '__main__':
+    main()
--- api_maldita/use_api.py
+++ api_maldita/use_api.py
@@ -1,6 +1,28 @@
 
 import requests
+import argparse
+import json
+import importlib.util
+spec = importlib.util.spec_from_file_location("credentials", "/home/blanca/prova/credentials.py")
+credentials = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(credentials)
+maldita_api = credentials.maldita_api
 
-response = requests.get("https://repositorio.iberifier.eu/api/contents?page=1&itemsPerPage=30", auth=requests.auth.HTTPBasicAuth('bcalvo', '5FybPQ8mD46vrRqN'))
+def get_arguments(parser):
+    parser.add_argument("--query", default='https://repositorio.iberifier.eu/api/contents?page=1&itemsPerPage=30', type=str, required=False)
+    return parser
 
-print(response.json())
+def main():
+    parser = argparse.ArgumentParser()
+    parser = get_arguments(parser)
+    args = parser.parse_args()
+    print(args)
+
+    user, key = maldita_api()
+    response = requests.get(args.query, auth=requests.auth.HTTPBasicAuth(user, key))
+
+    with open("results_{}.json".format('prova'), "w") as f:
+        f.write(json.dumps(response.json()))
+
+if __name__ == '__main__':
+    main()

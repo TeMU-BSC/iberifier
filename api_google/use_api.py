ec45e8de4bcf6e59184f123bb2c86d7c3b5100af
BlancaCalvo <bcalvo.bsc@gmail.com>
2022-04-14 17:22:01 +0200

    historical data google

--- api_google/use_api.py
+++ api_google/use_api.py
@@ -1,7 +1,9 @@
 from googleapiclient.discovery import build
 import json
 import argparse
+import time
 import os
+import pymongo
 import importlib.util
 spec = importlib.util.spec_from_file_location("credentials", os.getcwd()+"/credentials.py")
 credentials = importlib.util.module_from_spec(spec)
@@ -10,8 +12,32 @@ google_credentials = credentials.google_credentials
 
 
 def get_arguments(parser):
-    parser.add_argument("--query", default='vacunas', type=str, required=False)
+    parser.add_argument("--query", default='historical', type=str, required=False)
     return parser
+
+def historical_call(credentials):
+    myclient = pymongo.MongoClient('mongodb://127.0.0.1:27017/iberifier')
+    mydb = myclient.get_default_database()  # normalmente iberifier
+    mycol = mydb["google"]
+    #if mycol.count_documents({}) != 0:
+    #    print('There are entries already.')
+    #    exit()
+    all_letters = 'abcdefghijklmnopqrstuvwxyz'
+    for language in ['es', 'pt']:
+        # todo: pensar otra estrategia, creo que solo coge claims que contengan la letra aisaldamente como palabra
+        for letter in all_letters:
+            time.sleep(60)
+            request = credentials.claims().search(query=letter, pageSize=1000000, languageCode=language)
+            response = request.execute()
+            try:
+                data = response['claims']
+                print(data)
+                mycol.insert_many(data)
+            except:
+                continue
+    for post in mycol.find().limit(10):
+        print(post)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -20,17 +46,11 @@ def main():
     print(args)
 
     factCheckService = build("factchecktools", "v1alpha1", developerKey=google_credentials())
-    request = factCheckService.claims().search(query=args.query,
-                                                #reviewPublisherSiteFilter="Maldita.es",
-                                                pageSize=1000000,#)
-                                               languageCode="pt")# "es"
-                                               #pageToken='CAs',
-                                               # maxAgeDays='',
-                                               #offset=10)
-    response = request.execute()
 
-    with open("results_{}.json".format(args.query), "w") as f:
-        f.write(json.dumps(response))
+    if args.query == 'historical':
+        historical_call(factCheckService)
+
+    # todo: dar opción de recoger las claims diarias
 
 if __name__ == '__main__':
     main()

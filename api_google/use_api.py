342fbd33acbc9a3dbcc3d18aced2885ce25eca53
BlancaCalvo <bcalvo.bsc@gmail.com>
2022-04-21 17:21:20 +0200

    changed google historical and added daily

--- api_google/use_api.py
+++ api_google/use_api.py
@@ -5,6 +5,7 @@ import time
 import os
 import pymongo
 import importlib.util
+from nltk.corpus import stopwords
 spec = importlib.util.spec_from_file_location("credentials", os.getcwd()+"/credentials.py")
 credentials = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(credentials)
@@ -12,32 +13,75 @@ google_credentials = credentials.google_credentials
 
 
 def get_arguments(parser):
-    parser.add_argument("--query", default='historical', type=str, required=False)
+    parser.add_argument("--query", default='historical', type=str, required=False, help='\'historical\' gets all the data in the API, \'daily\' gets the fact-checks from today')
     return parser
 
-def historical_call(credentials):
-    myclient = pymongo.MongoClient('mongodb://127.0.0.1:27017/iberifier')
-    mydb = myclient.get_default_database()  # normalmente iberifier
-    mycol = mydb["google"]
-    #if mycol.count_documents({}) != 0:
-    #    print('There are entries already.')
-    #    exit()
-    all_letters = 'abcdefghijklmnopqrstuvwxyz'
+def historical_call(credentials, mycol):
     for language in ['es', 'pt']:
-        # todo: pensar otra estrategia, creo que solo coge claims que contengan la letra aisaldamente como palabra
-        for letter in all_letters:
-            time.sleep(60)
-            request = credentials.claims().search(query=letter, pageSize=1000000, languageCode=language)
+        queries = list(stopwords.words('spanish'))[120:] if language == 'es' else list(stopwords.words('portuguese'))
+        for q in queries:
+            print(q)
+            time.sleep(120)
+            request = credentials.claims().search(query=q, pageSize=1000000, languageCode=language)
             response = request.execute()
             try:
                 data = response['claims']
-                print(data)
+                print(len(data))
                 mycol.insert_many(data)
             except:
                 continue
     for post in mycol.find().limit(10):
         print(post)
 
+def daily_call(credentials, mycol):
+    # add here all the media coming from the historical data: db.google.distinct("claimReview.publisher.name");
+    list_media = [
+                  'AFP Factual',
+                  'Animal Político',
+                  'Antena 3',
+                  'Aos Fatos',
+                  'ColombiaCheck',
+                  'EFE Verifica - Agencia EFE',
+                  'Efecto Cocuyo',
+                  'El Surti',
+                  'El Surtidor (Paraguay)',
+                  'Europa Press',
+                  'FactCheck.org',
+                  'Fast Check CL',
+                  'Maldita.es',
+                  'Mexicana de Arte',
+                  'Newtral',
+                  'Polígrafo - SAPO',
+                  'Política - Estadão',
+                  'Telemundo',
+                  'UOL',
+                  'Univision',
+                  'Verificado',
+                  'Verificado MX',
+                  'Verificat',
+                  'elTOQUE'
+                ]
+
+    for media in list_media:
+        time.sleep(20)
+        print(media)
+        request = credentials.claims().search(reviewPublisherSiteFilter=media, maxAgeDays=1)
+        response = request.execute()
+        try:
+            data = response['claims']
+            print(len(data))
+            mycol.insert_many(data)
+        except:
+            continue
+
+def open_collection():
+    myclient = pymongo.MongoClient('mongodb://127.0.0.1:27017/iberifier')
+    mydb = myclient.get_default_database()  # normalmente iberifier
+    mycol = mydb["google"]
+    #if mycol.count_documents({}) != 0:
+    #    print('There are entries already.')
+    #    exit()
+    return mycol
 
 def main():
     parser = argparse.ArgumentParser()
@@ -46,11 +90,13 @@ def main():
     print(args)
 
     factCheckService = build("factchecktools", "v1alpha1", developerKey=google_credentials())
+    collection = open_collection()
 
     if args.query == 'historical':
-        historical_call(factCheckService)
+        historical_call(factCheckService, collection)
 
-    # todo: dar opción de recoger las claims diarias
+    elif args.query == 'daily':
+        daily_call(factCheckService, collection)
 
 if __name__ == '__main__':
     main()
--- api_maldita/use_api.py
+++ api_maldita/use_api.py
@@ -4,6 +4,7 @@ import argparse
 import json
 import os
 import pymongo
+import datetime
 import importlib.util
 spec = importlib.util.spec_from_file_location("credentials", os.getcwd()+"/credentials.py")
 credentials = importlib.util.module_from_spec(spec)
@@ -11,16 +12,10 @@ spec.loader.exec_module(credentials)
 maldita_credentials = credentials.maldita_credentials
 
 def get_arguments(parser):
-    parser.add_argument("--query", default='historical', type=str, required=False, help='\'historical\' gets all the data in the API, \'today\' gets the fact-checks from today')
+    parser.add_argument("--query", default='historical', type=str, required=False, help='\'historical\' gets all the data in the API, \'daily\' gets the fact-checks from today')
     return parser
 
-def historical_call(user, key):
-    myclient = pymongo.MongoClient('mongodb://127.0.0.1:27017/iberifier')
-    mydb = myclient.get_default_database()  # normalmente iberifier
-    mycol = mydb["maldita"]
-    if mycol.count_documents({}) != 0:
-        print('There are entries already.')
-        exit()
+def historical_call(user, key, mycol):
     page = 1
     while True:
         query = 'https://repositorio.iberifier.eu/api/contents?page='+str(page)+'&itemsPerPage=30'
@@ -35,6 +30,28 @@ def historical_call(user, key):
     for post in mycol.find().limit(10):
         print(post)
 
+def daily_call(user, key, mycol):
+    # get the data from the day before
+    yesterday = datetime.datetime.now()- datetime.timedelta(days=1)
+    string = yesterday.strftime('%Y-%m-%d')
+    query = 'https://repositorio.iberifier.eu/api/contents?page=1&itemsPerPage=30&createdAt%5Bstrictly_after%5D='+string
+    response = requests.get(query, auth=requests.auth.HTTPBasicAuth(user, key))
+    data = response.json()
+    print(len(data))
+    if data == []:
+        pass
+    else:
+        mycol.insert_many(data)
+
+def open_collection():
+    myclient = pymongo.MongoClient('mongodb://127.0.0.1:27017/iberifier')
+    mydb = myclient.get_default_database()  # normalmente iberifier
+    mycol = mydb["maldita"]
+    #if mycol.count_documents({}) != 0:
+    #    print('There are entries already.')
+    #    exit()
+    return mycol
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -43,11 +60,12 @@ def main():
     print(args)
 
     user, key = maldita_credentials()
+    collection = open_collection()
 
     if args.query == 'historical':
-        historical_call(user, key)
-
-    # TODO: add the option of retrieving the data daily
+        historical_call(user, key, collection)
+    elif args.query == 'daily':
+        daily_call(user, key, collection)
 
 if __name__ == '__main__':
     main()

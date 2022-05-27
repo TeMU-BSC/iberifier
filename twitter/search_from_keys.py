48241bd5264d10aeb5873813b951696151bf6237
Diego S <cuquiwi@hotmail.com>
2022-05-27 09:46:50 +0200

    Twitter search add temporal aspect

--- twitter/search_from_keys.py
+++ twitter/search_from_keys.py
@@ -1,11 +1,25 @@
 from twitter_search import TweetSearchUtil
 from pymongo import MongoClient
 import datetime
+from datetime import timedelta
 
-def search_twitter(query):
+MAX_TWEETS_RETRIEVED = 100 # for testing purpose limited to 100
+
+def search_twitter(query, date=None):
     tsu = TweetSearchUtil('../twittercredentials.yaml')
-    tweets = tsu.search_tweets_by_query(query, results_total=100, results_per_call=100, # for testing purpose limited to 100
+    if date == None:
+        tweets = tsu.search_tweets_by_query(query, results_total=MAX_TWEETS_RETRIEVED,
                                 tweet_fields='author_id,conversation_id,created_at,geo,id,lang,public_metrics,text')
+    else:
+        start_date = date - timedelta(days=30)
+        end_date = date + timedelta(days=30)
+        print(start_date.strftime("%Y-%m-%d %H:%M"))
+        # tweets = tsu.search_tweets_by_query(query, results_total=MAX_TWEETS_RETRIEVED,
+        #                         tweet_fields='author_id,conversation_id,created_at,geo,id,lang,public_metrics,text',
+        #                         start_time=start_date.strftime("%Y-%m-%d %H:%M"),
+        #                         end_time=end_date.strftime("%Y-%m-%d %H:%M")
+        #                         )
+
     return tweets
 
 def insert_tweets_mongo(tweets, source):
@@ -28,9 +42,10 @@ def main():
 
     sources_to_update = []
     # get only the documents who were not searched for
-    itercol = keywords_col.find({'searched_on':{'$exists':False}})
+    itercol = keywords_col.find({'searched_on':{'$exists':False}}).limit(3)
     for doc in itercol:
         news_id = doc['_id']
+        post_date = doc['createdAt']
         for key_list in doc['bigrams']:
             
             query = ' '.join(key_list) + ' -is:retweet'
@@ -41,7 +56,7 @@ def main():
                 query = ' '.join(key_list[:-i]) + ' -is:retweet'
                 i +=1
             
-            tweets = search_twitter(query)
+            tweets = search_twitter(query, post_date)
             insert_tweets_mongo(tweets, news_id)
 
         sources_to_update.append(news_id)

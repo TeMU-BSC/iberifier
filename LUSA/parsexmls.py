edcbf70cee85b4cef7bbb35397ca5ee567752776
Diego S <cuquiwi@hotmail.com>
2022-06-17 09:49:31 +0200

    Fix xml parsing insert in Mongo

--- LUSA/parsexmls.py
+++ LUSA/parsexmls.py
@@ -3,8 +3,6 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 
 import sys,os
-
-from bson import InvalidDocument
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from mongo_utils import mongo_utils
 
@@ -12,25 +10,22 @@ def main():
     items = []
     for file in glob.glob('./*/*.xml', recursive=True):
         print(file)
-        try:
-            xml = ET.parse(file)
-            root = xml.getroot().find('NewsItem')
-            
-            # Get Id and Date
-            iden = root.find('Identification')
-            
-            date = datetime.strptime(iden.find('DateLabel').text, '%d/%m/%Y %H:%M:%S')
-            id = iden.find('NewsIdentifier').find('NewsItemId').text
-            
-            # Get headline and content
-            compo = root.find('NewsComponent')
-            head = compo.find('NewsLines').find('HeadLine')
-            content = compo.find('ContentItem').find('DataContent')
-            
-            data = {'_id': id, 'date':date, 'headline':head, 'content':content}
-            items.append(data)
-        except InvalidDocument:
-            continue
+        xml = ET.parse(file)
+        root = xml.getroot().find('NewsItem')
+        
+        # Get Id and Date
+        iden = root.find('Identification')
+        
+        date = datetime.strptime(iden.find('DateLabel').text, '%d/%m/%Y %H:%M:%S')
+        id = iden.find('NewsIdentifier').find('NewsItemId').text
+        
+        # Get headline and content
+        compo = root.find('NewsComponent')
+        head = compo.find('NewsLines').find('HeadLine').text
+        content = compo.find('ContentItem').find('DataContent').text
+        
+        data = {'_id': id, 'date':date, 'headline':head, 'content':content}
+        items.append(data)
     
     mydb = mongo_utils.get_mongo_db()
     lusa_col = mydb['lusa']

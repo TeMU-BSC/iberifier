c79c5ecf3e70a3f65d82865e570bc9c8ae170fcf
BlancaCalvo <bcalvo.bsc@gmail.com>
2022-06-20 17:37:38 +0200

    added mynews sample

--- STS/dates_STS_filter.py
+++ STS/dates_STS_filter.py
@@ -21,17 +21,17 @@ logger.setLevel(logger_set_level)
 formatter = logging.Formatter("%(asctime)s :: %(levelname)s :: %(name)s :: %(message)s")
 
 def get_arguments(parser):
-    parser.add_argument("--claim", default='6278d46f875f753f342a4a76', type=str, required=False, help='give the id of a particular claim')
+    parser.add_argument("--claim", default=None, type=str, required=False, help='give the id of a particular claim')
     parser.add_argument("--source", default='telegram', type=str, required=False,
                         help='Fuente dónde estamos buscando textos relacionados: twitter_test, telegram, mynews, menéame or lusa')
-    parser.add_argument("--threshold", default=0.3, type=int, required=False,
+    parser.add_argument("--threshold", default=0.3, type=float, required=False,
                         help='Threshold of similarity.')
     parser.add_argument("--timeframe", default=7, type=int, required=False,
                         help='Up and down timeframe from the day of the claim.')
     return parser
 
 def select_messages_timeframe(time, messages, timeframe, source):
-    time = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S%z")
+    #time = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S%z")
     x = time - timedelta(days=timeframe)
     y = time + timedelta(days=timeframe)
     selection = list(messages.find({source['date']: {'$gt': x, '$lt': y}}))
@@ -93,8 +93,10 @@ def main():
 
     # look for one claim in the fact-check database
     collection_maldita = db_iberifier['maldita']
-    claim = collection_maldita.find_one({"_id": ObjectId(args.claim)})
-    #claim = collection_maldita.find_one({'createdAt': {'$gt': datetime(2021, 11, 1), '$lt': datetime(2021, 11, 30)}}) # I can't check Lusa until maldita has a datetime type
+    if args.claim:
+        claim = collection_maldita.find_one({"_id": ObjectId(args.claim)})
+    else:
+        claim = collection_maldita.find_one({'date': {'$gt': datetime(2021, 11, 1), '$lt': datetime(2021, 11, 30)}}) # I can't check Lusa until maldita has a datetime type
     print(claim)
 
     # TODO: make functions for each data source: telegram, twitter, LUSA and menéame, mynews (for the last to get the data)
@@ -109,28 +111,26 @@ def main():
             print('This data does not exist, we currently have the collections:') # TODO: show iberifier collections
             exit()
         else:
-            print('Not implemeted yet')
             source_keys = get_source_keys(args.source)
-            #exit()
 
-    # TODO: all sources should have a date field that is in datetime format
+    # TODO: all sources should have a date field that is in datetime format. Missing: google
     # get the messages in the time frame surrounding the claim
-    messages_in_frame = select_messages_timeframe(claim['createdAt'], collection_messages, args.timeframe, source_keys)
+    messages_in_frame = select_messages_timeframe(claim['date'], collection_messages, args.timeframe, source_keys)
     print(len(messages_in_frame))
     #for i in messages_in_frame:
     #    print(i)
 
     # evaluate how close the messages are and assess the threshold of similarity
-    model = SentenceTransformer('AIDA-UPM/MSTSb_stsb-xlm-r-multilingual')
+    model = SentenceTransformer('AIDA-UPM/mstsb-paraphrase-multilingual-mpnet-base-v2')
     c_vec = vectorize_claim(claim, model)
-    m_vecs = vectorize_messages(messages_in_frame, model)
+    m_vecs = vectorize_messages(messages_in_frame, model, source_keys)
     threshold = args.threshold
     relevant_indices = similar_messages(c_vec, m_vecs, threshold)
     print(len(relevant_indices))
 
     for i, m in enumerate(messages_in_frame):
         if i in relevant_indices:
-            print(m['message'])
+            print(m[source_keys['text']])
 
 
 if __name__ == '__main__':

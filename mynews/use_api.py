4f7ab734262016c5ac9a44c81e0213f0d5df5988
BlancaCalvo <blancacalvofigueras@gmail.com>
2022-09-29 17:10:02 +0200

    working API script

--- mynews/use_api.py
+++ mynews/use_api.py
@@ -2,24 +2,59 @@
 import requests
 import os
 import importlib.util
+import argparse
 
 cred_path = os.path.join(os.path.dirname(__file__), "../credentials.py")
 spec = importlib.util.spec_from_file_location("credentials", cred_path)
 credentials = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(credentials)
-credentials = credentials.mynews_credentials
+mynews_credentials = credentials.mynews_credentials
 
-key="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJvN3BtOElURWk4JjYxUXBJIiwiaWF0IjoxNjY0MjY2ODgyLjkzNzQ1MywiZXhwIjoxNjY0MjcwNDgyLjkzNzQ1M30.9qNyQrsfQAR3JOa2dzR8zBgSHIalIIVqYzZkzExSq0g"
+def get_arguments(parser):
+    parser.add_argument("--query", default="(BSC AND BARCELONA) OR (BSC AND MADRID)", type=str, required=False, help="the tokens to query")
+    parser.add_argument("--fromD", default="1654321118", type=str, required=False)
+    parser.add_argument("--toD", default="1656913073", type=str, required=False)
+    parser.add_argument("--topic", default="13", type=str, required=False)
+    parser.add_argument("--max", default="2", type=str, required=False)
+    return parser
 
-url = 'https://api.mynews.es/api/hemeroteca/'
+def get_token():
+    public_key, password = mynews_credentials()
+    files = {
+        'public_key': (None, public_key),
+        'password': (None, password),
+    }
+    TOKEN = requests.post('https://api.mynews.es/api/token/', files=files)
+    return TOKEN
 
-data = {"query": "(BSC AND BARCELONA) OR (BSC AND MADRID)", "maxResults":2}
-header = {'Authorisation': key}
+def query(token, args):
+    headers = {
+        'Authorization': f"Bearer {token.text}",
+    }
 
-response = requests.post(url, headers=header, params=data)
+    files = {
+        'query': (None, args.query),
+        'fromTime': (None, args.fromD),
+        'toTime': (None, args.toD),
+        'agrupations': (None, args.topic),
+        'maxResults': (None, args.max),
+    }
 
-print(response)
+    response = requests.post('https://api.mynews.es/api/hemeroteca/', headers=headers, files=files)
 
-data = response.json()
+    return response.json()
 
-print(data)
+def main():
+    parser = argparse.ArgumentParser()
+    parser = get_arguments(parser)
+    args = parser.parse_args()
+    print(args)
+
+    token = get_token()
+    result = query(token, args)
+
+    for element in result['news']:
+        print(element['Title'])
+
+if __name__ == "__main__":
+    main()

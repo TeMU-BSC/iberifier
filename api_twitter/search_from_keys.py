1d459f62738bcc826ea08ae47c768134305326fe
Oliph <olivier.philippe@gmail.com>
2022-10-06 14:28:17 +0200

    modify twitter api

--- api_twitter/search_from_keys.py
+++ api_twitter/search_from_keys.py
@@ -1,93 +1,116 @@
-from twitter_search import TweetSearchUtil
+import logging
+import os
+import sys
 from datetime import datetime
-from datetime import timedelta
 
-import sys, os
+import yaml
 
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from api_twitter import search_twitter
 from mongo_utils import mongo_utils
 
-# for testing purpose limited these are limited now
-MAX_TWEETS_RETRIEVED = 500
-MAX_CLAIMS_PER_DAY = 1
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 
-def search_twitter(query, date=None):
-    tsu = TweetSearchUtil("twittercredentials.yaml")
-    if date == None:
-        tweets = tsu.search_tweets_by_query(
-            query,
-            results_total=MAX_TWEETS_RETRIEVED,
-            tweet_fields="author_id,conversation_id,created_at,geo,id,lang,public_metrics,text",
-        )
-    else:
-        start_date = date + timedelta(days=-30)
-        end_date = date + timedelta(days=30)
-        tweets = tsu.search_tweets_by_query(
-            query,
-            results_total=MAX_TWEETS_RETRIEVED,
-            tweet_fields="author_id,conversation_id,created_at,geo,id,lang,public_metrics,text",
-            start_time=start_date.strftime("%Y-%m-%d %H:%M"),
-            end_time=end_date.strftime("%Y-%m-%d %H:%M"),
-        )
+logger = logging.getLogger(__name__)
 
-    return tweets
+# Load config and credentials
+
+config_all = yaml.safe_load(open("../config_files/config.yaml"))
+
+mongo_cred_path = os.path.join(
+    os.path.dirname(__file__),
+    "../config_files",
+    config_all["mongodb_params"]["mongodb_cred_filename"],
+)
+mongodb_credentials = yaml.safe_load(open(mongo_cred_path))[
+    "mongodb_credentials"]
+
+twitter_cred_path = os.path.join(
+    os.path.dirname(__file__),
+    "../config_files",
+    config_all["api_twitter_params"]["twitter_cred_filename"],
+)
+twitter_credentials = yaml.safe_load(open(twitter_cred_path))[
+    "search_tweets_api"]
 
 
-def insert_tweets_mongo(tweets, source):
-    mydb = mongo_utils.get_mongo_db()
-    tweets_col = mydb["twitter_test"]
+def insert_tweets_mongo(tweet, collection):
 
-    for t in tweets:
-        # Set the twitter id as the mongo id
-        t["_id"] = t["id"]
-        t["source"] = source
-    print(len(tweets))
-    tweets_col.insert_many(tweets)
+    collection.update_one({"id": tweet["id"]}, {"$set": tweet}, upsert=True)
 
 
 def main():
     # Iterate through collection
     mydb = mongo_utils.get_mongo_db()
-    keywords_col = mydb["maldita"]
+    cols_factcheckers = ['maldita', 'google']
+    col_tweets = config_all['mongodb_params']['col_tweets']
+
+    keyword_pairs_key = config_all['keywords_params']['keywords_pair_keys']
+
+    max_claims_per_day = config_all['api_twitter_params']['max_claims_per_day']
+    twitter_search_params = config_all['api_twitter_params']['search_params']
+    twitter_rule_params = config_all['api_twitter_params']['rule_params']
+    twitter_additional_query = twitter_search_params['additional_query']
+    twitter_additional_query = ' '.join(twitter_additional_query)
 
     sources_to_update = []
+
     # get only the documents who were not searched for
-    itercol = keywords_col.find(
-        {"searched_on": {"$exists": False}, "bigrams": {"$ne": None}}
-    ).limit(MAX_CLAIMS_PER_DAY)
+    for col in cols_factcheckers:
+        itercol = mydb[col].find(
+            {"searched_on": {"$exists": False},
+             keyword_pairs_key: {"$ne": None}}
+        ).limit(max_claims_per_day)
 
-    for doc in itercol:
-        news_id = doc["_id"]
-        print(news_id)
-        post_date_str = doc["createdAt"]
-        post_date = datetime.strptime(post_date_str, "%Y-%m-%dT%H:%M:%S%z")
+        for doc in itercol:
+            news_id = doc["_id"]
+            print(news_id)
+            post_date_str = doc["createdAt"]
+            post_date = datetime.strptime(post_date_str, "%Y-%m-%dT%H:%M:%S%z")
+            twitter_search_params['date'] = post_date
+            keyword_pairs = doc[keyword_pairs_key]
 
-        i = 0
-        while i < len(doc["bigrams"]):
+            i = 0
+            while i < len(keyword_pairs):
 
-            query = " ".join(doc["bigrams"][i])
-            newquery = query
-            i += 1
-            # Add bigrams to query until it reaches the 1024 query limit
-            # or all bigrams are added
-            while i < len(doc["bigrams"]) and len(newquery) < 1024 - (
-                len("() -is:retweet")
-            ):
-                query = newquery
-                newquery += " OR " + " ".join(doc["bigrams"][i])
+                query = " ".join(keyword_pairs[i])
+                newquery = query
                 i += 1
+                # Add bigrams to query until it reaches the 1024 query limit
+                # or all bigrams are added
+                # TODO include a different strategy otherwise the keywords will be the first one only
+                # TODO or include that in the keywords_processor script
+                while i < len(keyword_pairs) and len(newquery) < 1024 - (
+                    len("() {}".format(twitter_additional_query))
+                ):
+                    query = newquery
+                    newquery += " OR " + " ".join(doc[keyword_pairs][i])
+                    i += 1
 
-            query = "(" + query + ") -is:retweet"
+                # query = "(" + query + ") -is:retweet"
 
-            tweets = search_twitter(query, post_date)
-            insert_tweets_mongo(tweets, news_id)
+                # tweets = search_twitter(query, post_date)
+                # insert_tweets_mongo(tweets, news_id)
 
-        sources_to_update.append(news_id)
+                tweets = search_twitter(
+                    twitter_credentials, query, search_params=twitter_search_params, rule_params=twitter_rule_params)
 
-    keywords_col.update_many(
-        {"_id": {"$in": sources_to_update}}, {"$set": {"searched_on": datetime.now()}}
-    )
+                for tweet in tweets:
+                    insert_tweets_mongo(tweet, col_tweets)
+
+            sources_to_update.append(news_id)
+
+        mydb[col].update_many(
+            {"_id": {"$in": sources_to_update}}, {
+                "$set": {"searched_on": datetime.now()}}
+        )
+
+
+def main():
+    mydb = mongo_utils.get_mongo_db()
+    col_tweet_name = config_all["mongodb_params"]["col_name"]
+    col_tweets = mydb[col_tweet_name]
+    col_tweets.create_index([("id", 1)], unique=True)
 
 
 if __name__ == "__main__":

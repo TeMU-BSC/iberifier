e754a2df71dc29d79d44681221437d41847dcd73
Oliph <olivier.philippe@gmail.com>
2022-10-11 12:09:47 +0200

    adapt google_api with config yaml structure

--- api_google/use_api.py
+++ api_google/use_api.py
@@ -1,10 +1,13 @@
+import logging
+from mongo_utils import mongo_utils
+import sys
 from googleapiclient.discovery import build
 import argparse
 import time
 import os
+import yaml
 import importlib.util
 import datetime
-from nltk.corpus import stopwords
 
 cred_path = os.path.join(os.path.dirname(__file__), "../credentials.py")
 spec = importlib.util.spec_from_file_location("credentials", cred_path)
@@ -12,11 +15,20 @@ credentials = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(credentials)
 google_credentials = credentials.google_credentials
 
-import sys
+logger = logging.getLogger(__name__)
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from mongo_utils import mongo_utils
 
+config_path = os.path.join(os.path.dirname(__file__), '../config', 'config.yaml')
+config_all = yaml.safe_load(open(config_path))
+
+google_cred_path = os.path.join(
+    os.path.dirname(__file__),
+    "../config",
+    config_all["api_google_params"]["google_cred_filename"],
+)
+google_credentials = yaml.safe_load(open(google_cred_path))[
+    "google_api_credentials"]
 
 def get_arguments(parser):
     parser.add_argument(
@@ -29,12 +41,7 @@ def get_arguments(parser):
     return parser
 
 
-def historical_call(credentials, mycol):
-    list_media = [
-        "antena3.com",
-        "europapress.es",
-        "newtral.es",
-    ]
+def historical_call(credentials, mycol, list_media):
     # queries = list(stopwords.words('spanish'))[120:] if language == 'es' else list(stopwords.words('portuguese'))
     for media in list_media:
         request = credentials.claims().search(
@@ -43,69 +50,70 @@ def historical_call(credentials, mycol):
         response = request.execute()
         try:
             data = response["claims"]
-            print(len(data))
+            logger.info('Number of claims: {}'.format(len(data)))
             for element in data:
-                print(element['claimReview'][0]['reviewDate'])
+                # print(element['claimReview'][0]['reviewDate'])
                 element['date'] = datetime.datetime.strptime(element['claimReview'][0]['reviewDate'],
                                                              '%Y-%m-%dT%H:%M:%S%z')
             mycol.insert_many(data)
-        except:
+        except Exception as e:
+            logger.error(e)
             continue
+    logger.info('The 10 first posts inserted')
     for post in mycol.find().limit(10):
-        print(post)
+        logger.info(post)
 
 
-def daily_call(credentials, mycol):
+def daily_call(credentials, mycol, list_media):
     # add here all the media coming from the historical data: db.google.distinct("claimReview.publisher.name");
-    list_media = [
-        "antena3.com",
-        "europapress.es",
-        "newtral.es",
-    ]
 
     for media in list_media:
-        print('Resting 20 seconds.')
-        time.sleep(20)
         request = credentials.claims().search(
             reviewPublisherSiteFilter=media, maxAgeDays=3, languageCode="es"
         )
         response = request.execute()
         try:
             data = response["claims"]
-            print(len(data))
+            logger.info('Number of claims: {}'.format(len(data)))
             for element in data:
-                element['date'] = datetime.datetime.strptime(element['claimReview'][0]['reviewDate'], '%Y-%m-%dT%H:%M:%S%z')
+                element['date'] = datetime.datetime.strptime(
+                    element['claimReview'][0]['reviewDate'], '%Y-%m-%dT%H:%M:%S%z')
             mycol.insert_many(data)
-        except:
+        except Exception as e:
+            logger.error(e)
             continue
+        logger.info('Resting 20 seconds.')
+        time.sleep(20)
 
 
 def open_collection(new=False):
     mydb = mongo_utils.get_mongo_db()
-    mycol = mydb["google"]
-    if mycol.count_documents({}) != 0 and new == True:
-        print("There are entries already.")
+    col_google = config_all['mongodb_params']['google']['name']
+    collection = mydb[col_google]
+    if collection.count_documents({}) != 0 and new is True:
+        logger.info("There are entries already. Exiting")
         exit()
-    return mycol
+    return collection
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser = get_arguments(parser)
     args = parser.parse_args()
-    print(args)
-
+    logger.info('Arguments passed: {}'.format(args))
+    google_credentials_key = google_credentials['GOOGLE_API_KEY']
+    list_medias = config_all['api_google_params']['list_media'] 
     factCheckService = build(
-        "factchecktools", "v1alpha1", developerKey=google_credentials()
+        "factchecktools", "v1alpha1", developerKey=google_credentails_key(
     )
 
     if args.query == "historical":
         collection = open_collection(new=True)
-        historical_call(factCheckService, collection)
+        historical_call(factCheckService, collection, list_media)
 
     elif args.query == "daily":
         collection = open_collection(new=False)
-        daily_call(factCheckService, collection)
+        daily_call(factCheckService, collection, list_media)
 
 
 if __name__ == "__main__":

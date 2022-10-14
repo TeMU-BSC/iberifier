681be2aa2828c95e261a2c485a839f2df2dbaa30
Olivier Philippe <olivier.philippe@gmail.com>
2022-10-14 11:40:35 +0200

    working on changing the api_maldita

--- api_maldita/use_api.py
+++ api_maldita/use_api.py
@@ -1,20 +1,31 @@
 import requests
 import argparse
 import os
+import yaml
 import datetime
-import importlib.util
-
-cred_path = os.path.join(os.path.dirname(__file__), "../credentials.py")
-spec = importlib.util.spec_from_file_location("credentials", cred_path)
-credentials = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(credentials)
-maldita_credentials = credentials.maldita_credentials
+import logging
 
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from mongo_utils import mongo_utils
 
+
+
+config_path = os.path.join(os.path.dirname(
+    __file__), '../config', 'config.yaml')
+config_all = yaml.safe_load(open(config_path))
+
+maldita_cred_path = os.path.join(
+    os.path.dirname(__file__),
+    "../config",
+    config_all["api_maldita_params"]["cred_filename"],
+)
+maldita_credentials = yaml.safe_load(open(maldita_cred_path))[
+    "maldita_api_credentials"]
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def get_arguments(parser):
     parser.add_argument(
@@ -30,12 +41,11 @@ def get_arguments(parser):
 def historical_call(user, key, mycol):
     page = 1
     while True:
-        query = (
-            "https://repositorio.iberifier.eu/api/contents?page="
-            + str(page)
-            + "&itemsPerPage=30"
-        )
-        response = requests.get(query, auth=requests.auth.HTTPBasicAuth(user, key))
+        query = "https://repositorio.iberifier.eu/api/contents?page="
+        
+        params = {'page': str(page), 'itemPerPage': 30}
+        response = requests.get(
+            query, params=params, auth=requests.auth.HTTPBasicAuth(user, key))
         page += 1
         data = response.json()
         if data == []:
@@ -43,10 +53,12 @@ def historical_call(user, key, mycol):
         # with open("results_{}.jsonl".format('prova'), "a") as f:
         #    f.write(json.dumps(data) + "\n")
         for element in data:
-            element['date'] = datetime.datetime.strptime(element['createdAt'], '%Y-%m-%dT%H:%M:%S%z')
+            element['date'] = datetime.datetime.strptime(
+                element['createdAt'], '%Y-%m-%dT%H:%M:%S%z')
         mycol.insert_many(data)
+    logging.info('10th posts')
     for post in mycol.find().limit(10):
-        print(post)
+        logging.info(post)
 
 
 def daily_call(user, key, mycol):
@@ -59,19 +71,20 @@ def daily_call(user, key, mycol):
     )
     response = requests.get(query, auth=requests.auth.HTTPBasicAuth(user, key))
     data = response.json()
-    print(len(data))
+    logging.info(f'Number of claims collected: {len(data)}')
     if data == []:
         pass
     else:
         for element in data:
-            element['date'] = datetime.datetime.strptime(element['createdAt'], '%Y-%m-%dT%H:%M:%S%z')
-        #print(data)
+            element['date'] = datetime.datetime.strptime(
+                element['createdAt'], '%Y-%m-%dT%H:%M:%S%z')
+        # print(data)
         mycol.insert_many(data)
 
 
-def open_collection():
+def open_collection(col_name):
     mydb = mongo_utils.get_mongo_db()
-    mycol = mydb["maldita"]
+    mycol = mydb[col_name]
 
     return mycol
 
@@ -80,15 +93,19 @@ def main():
     parser = argparse.ArgumentParser()
     parser = get_arguments(parser)
     args = parser.parse_args()
-    print(args)
+    logging.info(f'Arguments passed: {args}')
 
+    col_maldita = config_all['mongodb_params']['maldita']['name']
+    collection = open_collection(col_maldita)
+    api_user = maldita_credentials['MALDITA_API_USER']
+    api_key = maldita_credentials['MALDITA_API_KEY']
     user, key = maldita_credentials()
     collection = open_collection()
 
     if args.query == "historical":
-        historical_call(user, key, collection)
+        historical_call(api_user, api_key, collection)
     elif args.query == "daily":
-        daily_call(user, key, collection)
+        daily_call(api_user, api_key, collection)
 
 
 if __name__ == "__main__":

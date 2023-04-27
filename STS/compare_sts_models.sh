a0a5f1453d120aeb5c16c434dfb5eca72c4f5033
BlancaCalvo <blancacalvofigueras@gmail.com>
2023-04-27 15:50:55 +0200

    model comparison for classification

---
+++ STS/compare_sts_models.sh
@@ -0,0 +1,12 @@
+
+python sts_model_eval.py news xlm_multi
+python sts_model_eval.py tweets xlm_multi
+
+python sts_model_eval.py news distiluse_multi
+python sts_model_eval.py tweets distiluse_multi
+
+python sts_model_eval.py news spanish
+python sts_model_eval.py tweets spanish
+
+python sts_model_eval.py news supervised_sts
+python sts_model_eval.py tweets supervised_sts
--- STS/sts_model_eval.py
+++ STS/sts_model_eval.py
@@ -1,92 +1,162 @@
 
 import json
 import sys
-from sentence_transformers import SentenceTransformer
-from scipy import spatial
+from sentence_transformers import SentenceTransformer, util
+#from scipy import spatial
 import numpy as np
 import matplotlib.pyplot as plt
-#import seaborn as sns
+from transformers import pipeline, AutoTokenizer
+from sklearn.metrics import balanced_accuracy_score
+
+def prepare(sentence_pairs, tokenizer):
+    sentence_pairs_prep = []
+    for s1, s2 in sentence_pairs:
+        sentence_pairs_prep.append(f"{tokenizer.cls_token} {s1}{tokenizer.sep_token}{tokenizer.sep_token} {s2}{tokenizer.sep_token}")
+    return sentence_pairs_prep
+
+def choose_thresholds(upper_boundary, lower_boundary, trials):
+    thresholds = [lower_boundary, upper_boundary]
+    if upper_boundary >= lower_boundary:
+        bit = (upper_boundary-lower_boundary)/trials
+        threshold_attempt = lower_boundary
+        for i in range(trials-1):
+            threshold_attempt += bit
+            thresholds.append(threshold_attempt)
+    return thresholds
+
+def calculate_accuracy(threshold, values, labels):
+    predicted_labels = []
+    for v in values:
+        if v > threshold:
+            predicted_labels.append(1)
+        elif v <= threshold:
+            predicted_labels.append(0)
+    return balanced_accuracy_score(labels, predicted_labels)
+
+def maximize_accuracy(values, labels, trials=5):
+    negative_values = []
+    positive_values = []
+    for i, line in enumerate(labels):
+        if line == 0:
+            negative_values.append(values[i])
+        elif line == 1:
+            positive_values.append(values[i])
+    upper_boundary = np.mean(positive_values)
+    lower_boundary = np.mean(negative_values)
+    thresholds = choose_thresholds(upper_boundary, lower_boundary, trials)
+    best_accuracy = 0
+    best_threshold = 0
+    for t in thresholds:
+        accuracy = calculate_accuracy(t, values, labels)
+        if accuracy > best_accuracy:
+            best_threshold = t
+            best_accuracy = accuracy
+    return best_threshold, best_accuracy
 
 def main():
     source = sys.argv[1] # news tweets
     model_source = sys.argv[2]
+    inform = True
+    plot = True
 
     with open('../data/eval_'+source+'.jsonl') as f:
         data = []
         for line in f:
             data.append(json.loads(line))
 
-    # mapping_labels = {'Difunde la claim falsa': 1,
-    #                   'Es sobre la claim pero no la apoya': 0.75,
-    #                   'Es sobre el tema pero no sobre la misma claim': 0.50,
-    #                     'Es otro tema': 0.25,
-    #                     'Tweet ilegible': 0}
-    mapping_labels = {'The text disseminates the false claim':0,
+    ordinal_labels = {'The text disseminates the false claim':0,
                  'The text is about the claim, but it does not support it':0.25,
                  'The text is on topic but not about this precise claim':0.50,
                  'The text is on another topic':0.75,
                  'The text is not readable':1}
 
+    mapping_labels = {'The text disseminates the false claim': 1,
+                      'The text is about the claim, but it does not support it': 1,
+                      'The text is on topic but not about this precise claim': 1,
+                      'The text is on another topic': 0,
+                      'The text is not readable': 0}
+
+    print(model_source, source)
     if model_source == 'xlm_multi':
         model = SentenceTransformer('sentence-transformers/stsb-xlm-r-multilingual')
     elif model_source == 'distiluse_multi':
         model = SentenceTransformer('sentence-transformers/distiluse-base-multilingual-cased')
     elif model_source == 'spanish':
         model = SentenceTransformer('hiiamsid/sentence_similarity_spanish_es')
+    elif model_source == 'supervised_sts':
+        path_sts = '../../models/roberta-base-bne-sts'
+        tokenizer_sts = AutoTokenizer.from_pretrained(path_sts)
+        model = pipeline('text-classification', model=path_sts, tokenizer=tokenizer_sts, truncation=True)
+        # model_te = pipeline("text-classification", model="PlanTL-GOB-ES/roberta-large-bne-te")
     else:
         print('There is no such model.')
         exit()
 
-    #claim_ids = [line["id_claim"] for line in data]
-    #unique_claim_ids = list(set(claim_ids))
     all_sts_values = []
-    #all_label_values = []
-    for label in mapping_labels.keys():
+    all_label_values = []
+    all_ordinal = []
+    appended_all_sts_values = []
+    for label, value in mapping_labels.items():
         sts_values = []
         label_values = []
-        for line in data:
-            if label == line['label_eng']:
-                embeddings = model.encode([line['claim'], line['text']])
-                dist = spatial.distance.cosine(embeddings[0], embeddings[1])
-                sts_values.append(dist)
-                #sts_values.append(1)
-                label_values.append(mapping_labels[line['label_eng']])
-        print(label)
-        print(len(sts_values))
-        print(np.mean(sts_values))
-        #all_sts_values.extend(sts_values)
-        all_sts_values.append(sts_values)
-        #all_label_values.extend(label_values)
-    #print(np.corrcoef(all_sts_values, all_label_values))
-
-    # plt.scatter(
-    #     x=all_sts_values,
-    #     y=all_label_values,
-    #     c=all_lab,
-    # )
-    #plt.yticks(list(mapping_labels.values()),
-    #           list(mapping_labels.keys()))
-
-    f = plt.figure()
-    f.set_figwidth(9)
-    plt.rc('xtick', labelsize=6)
-    violin_plot = plt.violinplot(all_sts_values[::-1], vert = False, showmedians=True)
-    if source == 'news':
-        plt.xlabel('Distance from Claim to Title')
-    else:
-        plt.xlabel('Distance from Claim to Tweet')
-    #plt.ylabel('Distance from Claim to Title')
-    plt.yticks([1,2,3,4,5],
-               list(mapping_labels.keys())[::-1])
-    for i, pc in enumerate(violin_plot["bodies"], 1):
-        if i <= 2:
-            pc.set_facecolor('#101E4A')
+        ordinal = []
+        if model_source != 'supervised_sts':
+            for line in data:
+                if label == line['label_eng']:
+                    #embeddings = model.encode([line['claim'], line['text']])
+                    #dist = spatial.distance.cosine(embeddings[0], embeddings[1])
+                    embeddings = model.encode([line['claim'], line['text']], convert_to_tensor=True)
+                    sim = util.cos_sim(embeddings[0], embeddings[1])
+                    sts_values.append(float(sim))
+                    #sts_values.append(1)
+                    label_values.append(mapping_labels[line['label_eng']])
+                    ordinal.append(ordinal_labels[line['label_eng']])
         else:
-            pc.set_facecolor('#FFDD4A')
-        pc.set_alpha(0.8)
-        pc.set_edgecolor('grey')
+            sentences = []
+            for line in data:
+                if label == line['label_eng']:
+                    sentences.append((line['claim'], line['text']))
+                    label_values.append(mapping_labels[line['label_eng']])
+                    ordinal.append(ordinal_labels[line['label_eng']])
+            predictions = model(prepare(sentences, tokenizer_sts), add_special_tokens=False)
+            sts_values = [p['score'] for p in predictions]
+            #sts_values = [1 for i in range(len(sentences))]
 
-    plt.savefig(source+'_'+model_source+".png", bbox_inches='tight', pad_inches = 0.05)
+
+        print(label, np.mean(sts_values), len(sts_values))
+        if inform:
+            all_sts_values.extend(sts_values)
+            all_label_values.extend(label_values)
+            all_ordinal.extend(ordinal)
+        if plot:
+            appended_all_sts_values.append(sts_values)
+
+    if inform:
+        print(np.corrcoef(all_sts_values, all_ordinal))
+        threshold, accuracy = maximize_accuracy(all_sts_values, all_label_values)
+        print(threshold, accuracy)
+
+    if plot:
+        f = plt.figure()
+        f.set_figwidth(9)
+        plt.rc('xtick', labelsize=6)
+        violin_plot = plt.violinplot(appended_all_sts_values[::-1], vert = False, showmedians=True)
+        if source == 'news':
+            plt.xlabel('Similarity between Claim and Title')
+        else:
+            plt.xlabel('Similarity between Claim and Tweet')
+
+        plt.yticks([1,2,3,4,5],
+                   list(mapping_labels.keys())[::-1])
+        for i, pc in enumerate(violin_plot["bodies"], 1):
+            if i <= 2:
+                pc.set_facecolor('#101E4A')
+            else:
+                pc.set_facecolor('#FFDD4A')
+            pc.set_alpha(0.8)
+            pc.set_edgecolor('grey')
+
+        plt.savefig(source+'_'+model_source+".png", bbox_inches='tight', pad_inches = 0.05)
 
 
 
